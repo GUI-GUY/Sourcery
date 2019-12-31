09b5895d04d1feac8d0cba9d9973a9f663894bcd
H-GUI <54667521+H-GUI@users.noreply.github.com>
2019-12-31 16:39:51 +0100

    experiments

--- create_GUI.py
+++ create_GUI.py
@@ -1,18 +1,21 @@
 from tkinter import Tk, W, E, IntVar
-from tkinter.ttk import Label, Checkbutton, Progressbar, Button
+from tkinter.ttk import Label, Checkbutton, Progressbar, Button, Style
 from tkinter import messagebox as mb
 from tkinter.filedialog import askdirectory
 from PIL import ImageTk, Image, ImageDraw
 import os
 import time
-from get_source import get_source_data 
+
+from upload_file import get_url
+#from get_source import get_source_data 
 
 def meth():
+    return
     for img in input_images_array:
-        print(get_source_data(cwd + "/test_images/" + img))
+        #print(get_source_data(cwd + "/test_images/" + img))
+        url = get_url(cwd, img)
         time.sleep(1)
     print(input_images_array)
-
 
 def is_image(img):
     """Returns True if the given images ends with a common image suffix such as .png or .jpg\n
@@ -40,19 +43,32 @@ def init_window():
 if __name__ == '__main__':
     #freeze_support()
 
+    #https://anonymousfiles.io/f/19455311_p0_kXX3cIh.jpg
+    #https://anonymousfiles.io/f/19455311_p0_V4i0IE5.jpg
+
     cwd = os.getcwd()
     window = Tk()
     window.title("Sourcery")
     window.update_idletasks()
-    window.state('zoomed')
+    #window.state('zoomed')
     height = window.winfo_screenheight()
     width = window.winfo_screenwidth()
-    window.geometry(str(width) + 'x' + str(height))
+    window.geometry(str(width-500) + 'x' + str(height-500))
     #dateS =  time.strftime("20%y-%m-%d")
 
+    # set style
+    window.configure(bg="#252525")
+    style = Style()
+    style.configure("label.TLabel", foreground="#ddd", background="#252525")
+    style.configure("button.TLabel", foreground="#ddd", background="#444")
+    style.map("button.TLabel",
+        foreground=[('pressed', 'red'), ('active', 'blue')],
+        background=[('pressed', '!disabled', '#111'), ('active', 'white')]
+    )
+    
     # widgets for start screen
-    sourcery_lbl = Label(window, text="Sourcery", font=("Arial Bold", 50))
-    method_name_or_text_here_btn = Button(window, text="Do stuff", command=meth)
+    sourcery_lbl = Label(window, text="Sourcery", font=("Arial Bold", 50), style="label.TLabel")
+    method_name_or_text_here_btn = Button(window, text="Do the magic", command=meth, style="button.TLabel")
 
     # global arrays
     input_images_array = []
